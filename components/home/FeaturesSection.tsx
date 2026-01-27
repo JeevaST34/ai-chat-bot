b6f4e2c0747172e85a3b1c425cf5b09a7d5e1d09
JeevaST34 <jeevanandam@scopethinkers.com>
2026-01-27 18:16:38 +0530

    Update FeaturesSection.tsx

--- components/home/FeaturesSection.tsx
+++ components/home/FeaturesSection.tsx
@@ -12,7 +12,7 @@ export function FeaturesSection() {
           title="Introducing AI Chatbot, your AI-powered site search"
           subtitle="Boost conversions and lighten the load on your support team"
           buttonText="Start optimizing your site for free"
-          image="/images/home/margin.png"
+          image="/images/home/Margin.png"
         />
         {/* GRID */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-20">
